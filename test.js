3120776a03c13a50985724a9b5bfe0ab07c3dcd9
schahriar <info@schahriar.com>
2014-12-26 00:46:58 -0800

    Completely embedded ~culinary~ into herb
    Now it's time for testing & debugging

--- test.js
+++ test.js
@@ -2,10 +2,22 @@ var chai = require("chai");
 var herb = require("./herb");
 
 var _ = require("lodash");
-var chalk = require("chalk");
 
 var should = chai.should();
 var expect = chai.expect;
+
+var look = {
+	escape: "\\u001b[",
+	reset: "39m",
+	resetBold: "22m",
+	
+	blue: function(string){
+		return this.escape.concat("34m",string,this.escape,this.reset);
+	},
+	bold: function(string){
+		return this.escape.concat("1m",string,this.escape,this.resetBold);
+	}
+}
 
 herb.config({ verbose: 4, testSuite: true });
 describe('Test Suite', function(){
@@ -14,8 +26,8 @@ describe('Test Suite', function(){
 			herb.parse(function(){
 				var args = _.toArray(arguments);
 				
-				args[0].should.equal("\u001b[34mTest1\u001b[39m");
-				args[1].should.equal("\u001b[34mTest2\u001b[39m");
+				args[0].should.equal(look.blue("Test1"));
+				args[1].should.equal(look.blue("Test2"));
 				
 				done();
 			}, "Test1", "Test2");
@@ -23,63 +35,66 @@ describe('Test Suite', function(){
 
 		it('should accept JSON', function(done){
 			herb.parse(function(){
-                                var args = _.toArray(arguments);
-				
+				var args = _.toArray(arguments);
+
 				var sampleFunction = function x(){};
 
-                                args[0].should.equal("\u001b[34m{\n      \"test\": \"test\",\n      \"inner\": {\n          \"one\": true\n      },\n      \"array\": [\n          2,\n          3,\n          4\n      ]\n  }\u001b[39m");
-                                args[1].should.equal("\u001b[34m[\n      1,\n      2,\n      3\n  ]\u001b[39m");
-				args[2].should.equal(sampleFunction.toString());
+				args[0].should.equal(look.blue("{\n      \"test\": \"test\",\n      \"inner\": {\n          \"one\": true\n      },\n      \"array\": [\n          2,\n          3,\n          4\n      ]\n  }"));
 				
+				args[1].should.equal(look.blue("[\n      1,\n      2,\n      3\n  ]"));
+				args[2].should.equal(sampleFunction.toString());
+
 				done();
-                        }, {test:'test', inner:{one:true}, array:[2,3,4]}, [1,2,3], function x(){});
+			}, {test:'test', inner:{one:true}, array:[2,3,4]}, [1,2,3], function x(){});
 		})
 
 		it('should accept multi-arguments', function(done){
 			herb.parse(function(){
-                                var args = _.toArray(arguments);
+				var args = _.toArray(arguments);
 
-                                args[0].should.equal("\u001b[34mtest\u001b[39m");
-                                args[1].should.equal(2.53);
-				args[2].should.equal("\u001b[34m[\n      53,\n      5,\n      5\n  ]\u001b[39m");
-				args[3].should.equal("\u001b[34mone\u001b[39m");
-				args[4].should.equal("\u001b[34m{\n      \"one\": true\n  }\u001b[39m");
+				args[0].should.equal(look.blue("test"));
+				args[1].should.equal(2.53);
+				args[2].should.equal(look.blue("[\n      53,\n      5,\n      5\n  ]"));
+				args[3].should.equal(look.blue("one"));
+				args[4].should.equal(look.blue("{\n      \"one\": true\n  }"));
 
-                                done();
-                        }, "test", 2.53, [53,5,5], "one", {one:true});
-                })
+				done();
+			}, "test", 2.53, [53,5,5], "one", {one:true});
+		})
 
-                it('should not return log when verbosity < 0', function(done){
-                        herb.config({ verbose: -1 })
+		it('should not return log when verbosity < 0', function(done){
+			herb.config({ verbose: -1 })
 			herb.parse(function(){
-                                var args = _.toArray(arguments);
+				var args = _.toArray(arguments);
 
-                                expect(args[0]).to.equal(undefined);
+				expect(args[0]).to.equal(undefined);
 
-                                done();
-                        }, "Test1");
-                })
+				done();
+			}, "Test1");
+		})
+		
 		it('should display groups correctly', function(done){
-                        herb.config({ verbose: 0 });
+			herb.config({ verbose: 0 });
 			herb.group("New Group");
-                        herb.parse(function(){
-                                var args = _.toArray(arguments);
+			herb.parse(function(){
+				var args = _.toArray(arguments);
 
-                                args[0].should.equal("\u001b[1m| \u001b[22m"); 
-				args[1].should.equal("\u001b[34mGroup Item\u001b[39m");
-                                done();
-                        }, "Group Item");
-                })
+				args[0].should.equal(look.bold("| ")); 
+				args[1].should.equal(look.blue("Group Item"));
+				done();
+			}, "Group Item");
+		})
+		
 		it('should display nested groups correctly', function(done){
-                        herb.group("New Group 2");
-                        herb.parse(function(){
-                                var args = _.toArray(arguments);
+			herb.group("New Group 2");
+			herb.parse(function(){
+				var args = _.toArray(arguments);
 
-                                args[0].should.equal("\u001b[1m| | \u001b[22m");
-                                args[1].should.equal("\u001b[34mGroup Item\u001b[39m");
-                                done();
-                        }, "Group Item");
-                })
+				args[0].should.equal(look.bold("| | "));
+				args[1].should.equal(look.blue("Group Item"));
+				done();
+			}, "Group Item");
+		})
 	})
 
 });
